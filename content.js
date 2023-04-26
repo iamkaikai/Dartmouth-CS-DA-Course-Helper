1ad8a9539d02b469ae8bebca8ec8234eb1d39325
YENKAI HUANG <iamkaikai@livemail.tw>
2023-04-26 12:56:15 -0400

    update dragging interaction

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
+
 var panelTitle = document.createElement('div');
 var panelText = document.createElement('p');
 var panelImg = document.createElement('img');
@@ -7,35 +8,40 @@ var iframeText = document.createElement('p');
 var iframe = document.createElement('iframe');
 var imgURL1 = chrome.runtime.getURL("logo.svg");
 var imgURL2 = chrome.runtime.getURL("hover.png");
+var wrapperAll = document.createElement('div');
 
 panelTitle.setAttribute('id','panelTitle');
 panelImg.setAttribute('id','panelImg');
 panelText.setAttribute('id','panelText');
 iframeText.setAttribute('id', 'iframeText');
 iframeImg.setAttribute('id', 'iframeImg');
-iframeWrapper.setAttribute('id', 'iframeWapper');
+iframeWrapper.setAttribute('id', 'iframeWrapper');
 iframe.setAttribute('id', 'iframe');
+wrapperAll.setAttribute('id', 'wrapperAll');
 
-document.body.appendChild(panelTitle);
-document.body.appendChild(iframeWrapper);
+document.body.appendChild(wrapperAll);
+wrapperAll.appendChild(panelTitle);
+wrapperAll.appendChild(iframeWrapper);
+
 
 $("#panelTitle").append(panelImg);
 $("#panelTitle").append(panelText);
 $("#panelText").html("CS/DA Course Helper");
 $("#panelImg").attr('src',imgURL1);
-$("#iframeWapper").append(iframeImg);
-$("#iframeWapper").append(iframeText);
-$("#iframeWapper").append(iframe);
+$("#iframeWrapper").append(iframeImg);
+$("#iframeWrapper").append(iframeText);
+$("#iframeWrapper").append(iframe);
 $("#iframeImg").attr('src',imgURL2);
 $("#iframeText").html("Move the cursor on the course circle to get the outline of the course");
 
-$("#iframeWapper").css({"opacity":"0.95","background":"#114d33", "z-index": "9999","overflow":"hidden","position": "absolute","bottom":"0","left":"0","width":"528","height":"290","border":"#114d33 2px solid"});
-$("#panelTitle").css({ "z-index": "10000","width":"115px","height":"14px","bottom":"294px","left":"-2px","background":"#114d33","position":"absolute"});
-$("#panelText").css({"position": "absolute","top":"3px","display":"inline","margin":"0","color":"white","font-size":"9px"});
-$("#panelImg").css({"margin":"0 3px 0 6px","display":"inline",'width': '10px'});
+$("#iframeWrapper").css({"opacity":"0.95","background":"#114d33","overflow":"hidden","width":"530","height":"290","border":"#114d33 2px solid"});
+$("#panelTitle").css({ "width":"534px","height":"18px", "background":"#114d33"});
+$("#panelText").css({"position": "absolute","top":"4px","display":"inline","margin":"0","color":"white","font-size":"9px"});
+$("#panelImg").css({"margin":"4.5px 5px 0px 6px","display":"inline",'width': '10px'});
 $("#iframeText").css({"margin-top":"18px","color":"white","font-size":"13px","text-align":"center"});
 $("#iframeImg").css({"display": "block","margin":"auto","margin-top":"82px","width":"90px","text-align":"center"});
-$("#iframe").css({ "transform":"scale(0.8)", "width":"934px","height":"3000px","margin-top":"-920px","margin-left":"-314px","bottom":"-35px"});
+$("#iframe").css({ "transform":"scale(0.8)", "width":"937px","height":"3000px","margin-top":"-950px","margin-left":"-314px","bottom":"-35px"});
+$("#wrapperAll").css({ "border-radius":"5px", "overflow":"hidden", "width": "534px", "height": "313px", "z-index": "99", "bottom": "0", "position": "absolute", "left": "0"})
 
 $("body").on('DOMSubtreeModified', ".mxTooltip", function() {
     
@@ -65,4 +71,9 @@ $("body").on('DOMSubtreeModified', ".mxTooltip", function() {
     iframeLink();
 });
 
+$("#wrapperAll").draggable();
+$("#iframe").draggable();
+$("#panelTitle").resizable(console.log("resize") );
+$("#wrapperAll").resizable({ handle: "#panelTitle" });
+// $("#wrapperAll").resizable({ alsoResize: "#iframe" });
 
